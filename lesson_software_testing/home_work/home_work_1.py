e992a0d0fc7d8e5d3a9541de6e97c66e5f196f3b
Blazzer87 <nitros1987@gmail.com>
2024-11-19 09:03:12 +0300

    commit 1

--- lesson_software_testing/home_work/home_work_1.py
+++ lesson_software_testing/home_work/home_work_1.py
@@ -8,9 +8,10 @@ service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
 driver.get('https://airgun.org.ru/forum/index.php')
-time.sleep(3)                                           #таймер на 10 секунд чтобы увидеть станицу
-driver.back()                                           #навигация назад
+time.sleep(3)                                           # таймер на 3 секунды чтобы увидеть станицу
+driver.back()                                           # навигация назад
 time.sleep(3)
-driver.forward()                                        #навигация вперёд
+driver.forward()                                        # навигация вперёд
 time.sleep(3)
-driver.refresh()                                        #перезагрузка страницы
+driver.refresh()                                        # перезагрузка страницы
+driver.refresh()                                        # перезагрузка страницы
