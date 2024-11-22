ac00effcec05ffd3c611cbbc13cd3e26408e0340
Blazzer87 <nitros1987@gmail.com>
2024-11-22 17:16:17 +0300

    commit 3

--- reestr/reestr_positive_registration.py
+++ reestr/reestr_positive_registration.py
@@ -19,8 +19,16 @@ while user_input == 0:
     lastname.send_keys('Фролов')
     firstname = driver.find_element(By.CSS_SELECTOR, '[name=firstName]')
     firstname.send_keys('Миша')
+    middlename = driver.find_element(By.CSS_SELECTOR, '[name=middletName]')
+    middlename.send_keys('Васильевич')
+    phone = driver.find_element(By.CSS_SELECTOR, '[name=phone]')
+    phone.send_keys('79507586687')
+    inn = driver.find_element(By.CSS_SELECTOR, '[name=inn]')
+    inn.send_keys('871204050495')
+    snils = driver.find_element(By.CSS_SELECTOR, '[name=snils]')
+    snils.send_keys('12345678912')
 
-    time.sleep(10)
+    time.sleep(5)
 
     user_input = int(input("Если пробуем снова то введи 0?"))
 
