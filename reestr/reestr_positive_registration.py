eb62f2dc6bfd86802afee983f564a432e554be2d
Blazzer87 <nitros1987@gmail.com>
2024-11-22 17:18:24 +0300

    commit 3

--- reestr/reestr_positive_registration.py
+++ reestr/reestr_positive_registration.py
@@ -12,14 +12,14 @@ user_input = 0
 while user_input == 0:
 
     driver.get('https://reestr-hello-linux.qpdev.ru/')
-    time.sleep(2)
+    time.sleep(4)
     registration = driver.find_element(By.CSS_SELECTOR, '[class=qpd-rstr--register-0-2-54]')
     registration.click()
     lastname = driver.find_element(By.CSS_SELECTOR, '[name=lastName]')
     lastname.send_keys('Фролов')
     firstname = driver.find_element(By.CSS_SELECTOR, '[name=firstName]')
     firstname.send_keys('Миша')
-    middlename = driver.find_element(By.CSS_SELECTOR, '[name=middletName]')
+    middlename = driver.find_element(By.CSS_SELECTOR, '[name=middleName]')
     middlename.send_keys('Васильевич')
     phone = driver.find_element(By.CSS_SELECTOR, '[name=phone]')
     phone.send_keys('79507586687')
