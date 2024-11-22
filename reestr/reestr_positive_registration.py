51fa9de6d013705711ff92091a7129844a7c460a
Blazzer87 <nitros1987@gmail.com>
2024-11-22 17:01:01 +0300

    commit 3

--- reestr/reestr_positive_registration.py
+++ reestr/reestr_positive_registration.py
@@ -8,12 +8,21 @@ from selenium.webdriver.common.by import By
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
-driver.get('https://reestr-hello-linux.qpdev.ru/')
-registration = driver.find_element(By.CLASS_NAME, 'qpd-rstr--register-0-2-54')
-registration.click()
-lastname = driver.find_element(By.CSS_SELECTOR, '[name=lastName]')
-lastname.send_keys('Фролов')
-firstname = driver.find_element(By.CSS_SELECTOR, '[name=firstName]')
-firstname.send_keys('Миша')
+user_input = 0
+while user_input == 0:
 
-time.sleep(10)
+    driver.get('https://reestr-hello-linux.qpdev.ru/')
+    time.sleep(2)
+    registration = driver.find_element(By.CSS_SELECTOR, '[class=qpd-rstr--register-0-2-54]')
+    registration.click()
+    lastname = driver.find_element(By.CSS_SELECTOR, '[name=lastName]')
+    lastname.send_keys('Фролов')
+    firstname = driver.find_element(By.CSS_SELECTOR, '[name=firstName]')
+    firstname.send_keys('Миша')
+
+    time.sleep(10)
+
+    user_input = int(input("Если пробуем снова то введи 0?"))
+
+driver.quit()
+exit()
