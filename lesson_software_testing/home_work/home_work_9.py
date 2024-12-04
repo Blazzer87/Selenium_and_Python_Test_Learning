606a5e03012447293a2a35ea8499904c22648e52
Blazzer87 <nitros1987@gmail.com>
2024-12-04 17:29:54 +0300

    commit 3

---
+++ lesson_software_testing/home_work/home_work_9.py
@@ -0,0 +1,40 @@
+'''
+Сделайте сценарии, который на странице http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones
+заходит в каждую из стран и проверяет, что зоны расположены в алфавитном порядке.
+'''
+import time
+
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.devtools.v85.dom import get_attributes
+
+options = webdriver.ChromeOptions()                 # создаём опции хрома
+options.add_argument('start-maximized')             # передаём фулскрин в опции хрома
+driver = webdriver.Chrome(options)                  # запускаем хром с опциями переданными ранее
+
+driver.get('http://localhost/litecart/admin/login.php')
+driver.implicitly_wait(2)
+
+username = driver.find_element(By.CSS_SELECTOR, '[name=username]')
+username.send_keys('admin')
+password = driver.find_element(By.CSS_SELECTOR, '[name=password]')
+password.send_keys('admin')
+rememberme = driver.find_element(By.CSS_SELECTOR, '[name=remember_me]')
+rememberme.click()
+submit = driver.find_element(By.CSS_SELECTOR, '[name=login]')
+submit.click()
+
+countries = driver.find_element(By.XPATH, '//*[@id="app-"]//span[text()="Geo Zones"]')
+countries.click()
+canada = driver.find_element(By.XPATH, '//*[@id="content"]//a[text()="Canada"]')
+canada_html = canada.get_attribute('href')
+driver.get(canada_html)
+
+
+
+
+
+time.sleep(5)
+driver.quit()
+
