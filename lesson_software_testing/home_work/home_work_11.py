9395b41fdf938ffff79561cb7a9f40f9eca93b08
Blazzer87 <nitros1987@gmail.com>
2024-12-08 22:34:18 +0300

    домашка 7

--- lesson_software_testing/home_work/home_work_11.py
+++ lesson_software_testing/home_work/home_work_11.py
@@ -10,72 +10,107 @@
 В качестве страны выбирайте United States, штат произвольный. При этом формат индекса -- пять цифр.
 '''
 
-import time
+import faker
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
 options = webdriver.ChromeOptions()                 # создаём опции хрома
 options.add_argument('start-maximized')             # передаём фулскрин в опции хрома
+options.add_argument('incognito')                   # передаём инкогнито в опции хрома
 driver = webdriver.Chrome(options)                  # запускаем хром с опциями переданными ранее
 
 driver.get('http://localhost/litecart/en/')
 driver.implicitly_wait(2)
 
-reg_data = [
-    {'tax_id':'12345'},
-    {'company': 'QPD'},
-    {'firstname':'Sergey'},
-    {'lastname':'Laba'},
-    {'address1':'Pobedi 60'},
-    {'address2':'kv. 90'},
-    {'postcode':'55555'}
-    ]
+fake_email = faker.Faker().email()
+print("Используемая почта:", fake_email)
+
+reg_data = {
+    'tax_id':'454545',
+    'company': 'QPD',
+    'firstname':'Sergey',
+    'lastname':'Laba',
+    'address1':'Pobedi 60',
+    'address2':'kv. 90',
+    'postcode':'55555',
+    'city':'Voronezh',
+    'country':'United States',
+    'email':f'{fake_email}',
+    'phone':'79507586687',
+    'password':'!QAZ1qaz',
+    'confirmed_password':'!QAZ1qaz'
+    }
 
 registration_button = driver.find_element(By.XPATH, '//*[@id="box-account-login"]//a[text()="New customers click here"]')
 registration_button.click()
 
 tax_id = driver.find_element(By.XPATH, '//*[@id="create-account"]//input[@name="tax_id"]')
-tax_id.send_keys('12345')
+tax_id.send_keys(f'{reg_data.get('tax_id')}')
 
 company = driver.find_element(By.XPATH, '//*[@id="create-account"]//input[@name="company"]')
-company.send_keys('QPD')
+company.send_keys(f'{reg_data.get('company')}')
 
-first_name = driver.find_element(By.XPATH, '//*[@id="create-account"]//input[@name="firstname"]')
-first_name.send_keys('Sergey')
+firstname = driver.find_element(By.XPATH, '//*[@id="create-account"]//input[@name="firstname"]')
+firstname.send_keys(f'{reg_data.get('firstname')}')
 
-last_name = driver.find_element(By.XPATH, '//*[@id="create-account"]//input[@name="lastname"]')
-last_name.send_keys('Laba')
+lastname = driver.find_element(By.XPATH, '//*[@id="create-account"]//input[@name="lastname"]')
+lastname.send_keys(f'{reg_data.get('lastname')}')
 
 address1 = driver.find_element(By.XPATH, '//*[@id="create-account"]//input[@name="address1"]')
-address1.send_keys('Pobedi 60')
+address1.send_keys(f'{reg_data.get('address1')}')
 
 address2 = driver.find_element(By.XPATH, '//*[@id="create-account"]//input[@name="address2"]')
-address2.send_keys('kv. 90')
+address2.send_keys(f'{reg_data.get('address2')}')
 
 postcode = driver.find_element(By.XPATH, '//*[@id="create-account"]//input[@name="postcode"]')
-postcode.send_keys('55555')
+postcode.send_keys(f'{reg_data.get('postcode')}')
 
 city = driver.find_element(By.XPATH, '//*[@id="create-account"]//input[@name="city"]')
-city.send_keys('Voronezh')
+city.send_keys(f'{reg_data.get('city')}')
 
 email = driver.find_element(By.XPATH, '//*[@id="create-account"]//input[@name="email"]')
-email.send_keys('laba@laba.ru')
-
-password = driver.find_element(By.XPATH, '//*[@id="create-account"]//input[@name="password"]')
-password.send_keys('!QAZ1qaz')
-
-confirmed_password = driver.find_element(By.XPATH, '//*[@id="create-account"]//input[@name="confirmed_password"]')
-confirmed_password.send_keys('!QAZ1qaz')
+email.send_keys(f'{reg_data.get('email')}')
 
 phone = driver.find_element(By.XPATH, '//*[@id="create-account"]//input[@name="phone"]')
-phone.send_keys('79507586687')
+phone.send_keys(f'{reg_data.get('phone')}')
 
+password = driver.find_element(By.XPATH, '//*[@id="create-account"]//input[@name="password"]')
+password.send_keys(f'{reg_data.get('password')}')
 
+confirmed_password = driver.find_element(By.XPATH, '//*[@id="create-account"]//input[@name="confirmed_password"]')
+confirmed_password.send_keys(f'{reg_data.get('confirmed_password')}')
 
+country_selector = driver.find_element(By.CSS_SELECTOR, '[role=presentation]')
+country_selector.click()
 
+country_list = driver.find_element(By.CSS_SELECTOR, '[type=search]')
+country_list.send_keys(f'{reg_data.get('country')}')
+country_list.send_keys(Keys.ENTER)
 
+zone_code = driver.find_element(By.XPATH, '//*//*[@id="create-account"]//select[@name="zone_code"]')
+zone_code.click()
+zone = driver.find_element(By.XPATH, '//*[@id="create-account"]//select/option[text()="Nevada"]')
+zone.click()
+zone_code.click()
 
+create_account = driver.find_element(By.XPATH, '//*[@id="create-account"]//button[@type="submit"]')
+create_account.click()
 
-time.sleep(5)
+logout_button = driver.find_element(By.XPATH, '//*[@id="box-account"]//a[text()="Logout"]')
+logout_button.click()
+
+login_email = driver.find_element(By.XPATH, '//*[@id="box-account-login"]//input[@name="email"]')
+login_email.send_keys(fake_email)
+
+login_password = driver.find_element(By.XPATH, '//*[@id="box-account-login"]//input[@name="password"]')
+login_password.send_keys(reg_data.get('password'))
+
+login_button = driver.find_element(By.XPATH, '//*[@id="box-account-login"]//button[text()="Login"]')
+login_button.click()
+
+logout_button = driver.find_element(By.XPATH, '//*[@id="box-account"]//a[text()="Logout"]')
+logout_button.click()
+
 driver.quit()
 
---
+++ lesson_software_testing/test_2.py
@@ -0,0 +1,7 @@
+import faker
+
+f = faker.Faker()
+
+print(f.email())
+
+
