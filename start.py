ce4a2f03fcf18ae3fd82dc685878a7c65463ae33
Blazzer87 <nitros1987@gmail.com>
2025-01-16 00:30:38 +0300

    домашка 7

---
+++ start.py
@@ -0,0 +1,24 @@
+"""
+Установить виртуальное окружение
+python -m venv venv
+
+Активировать виртуальное окружение
+venv\Scripts\activate.ps1
+
+Установить библиотеки, набор будет меняться, перечислены наиболее используемые
+pip install pytest pydantic requests Faker python-dotenv allure-pytest
+
+Скопировать все установленные библиотеки в файл requirements
+pip freeze > requirements.txt
+
+Скачать все библиотеки из requirements
+pip install -r requirements.txt
+
+
+
+
+
+
+
+
+"""
